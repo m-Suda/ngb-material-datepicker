c0451ae779666c5754937b76a2bdf467cc6ce8eb
masaks <oicwam.orz1228@gmail.com>
2021-05-04 00:29:50 +0900

    プロジェクト初期構築

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgbMaterialDatepickerService } from '../../projects/ngb-material-datepicker/src/lib/ngb-material-datepicker.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bootstrap-material-datepicker-for-angular';
+    title = 'bootstrap-material-datepicker-for-angular';
+    constructor(private ngbMaterialDatepicker: NgbMaterialDatepickerService) {}
+
+    public openDatepicker() {
+        this.ngbMaterialDatepicker.open({
+            dateColor: '#A0A0A0',
+            headerColor: '#FFD900',
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbMaterialDatepickerModule } from '../../projects/ngb-material-datepicker/src/lib/ngb-material-datepicker.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, NgbMaterialDatepickerModule, BrowserAnimationsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
