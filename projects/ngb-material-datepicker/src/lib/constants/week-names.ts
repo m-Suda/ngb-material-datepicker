2bf3c41f655a75384b81cad7c3a9e3e43c475cf9
masaks <oicwam.orz1228@gmail.com>
2021-05-04 00:31:36 +0900

    曜日エイリアスから日本の曜日を取得するやつ作成

---
+++ projects/ngb-material-datepicker/src/lib/constants/week-names.ts
@@ -0,0 +1,11 @@
+import { WeekAlias } from '../types/week';
+
+export const JapaneseWeek: { [key in WeekAlias]: string } = {
+    0: '日曜日',
+    1: '月曜日',
+    2: '火曜日',
+    3: '水曜日',
+    4: '木曜日',
+    5: '金曜日',
+    6: '土曜日',
+};
