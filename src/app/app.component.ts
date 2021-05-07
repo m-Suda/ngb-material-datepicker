c37e03009184d9b83b07da921324cab928f8f66f
masaks <oicwam.orz1228@gmail.com>
2021-05-08 04:48:06 +0900

    動作確認用app.componentの修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,49 @@ export class AppComponent {
     title = 'bootstrap-material-datepicker-for-angular';
     constructor(private ngbMaterialDatepicker: NgbMaterialDatepickerService) {}
 
+    public normalResult = '';
+    public customColorResult = '';
+    public specifyResult = '';
+    public outputFormatResult = '';
+
     public openDatepicker() {
-        this.ngbMaterialDatepicker.open({});
+        const ref = this.ngbMaterialDatepicker.open({});
+        ref.afterClose.subscribe((r) => {
+            if (r == null) return;
+            this.normalResult = r;
+        });
     }
 
     public openDatepickerCustomColor() {
-        this.ngbMaterialDatepicker.open({
+        const ref = this.ngbMaterialDatepicker.open({
             dateColor: '#692bff',
+            dateTextColor: '#FFFFFF',
             headerColor: '#7e47ff',
+            headerTextColor: '#FFFFFF',
+        });
+        ref.afterClose.subscribe((r) => {
+            if (r == null) return;
+            this.customColorResult = r;
         });
     }
 
     public openDatepickerSpecify() {
-        this.ngbMaterialDatepicker.open({
+        const ref = this.ngbMaterialDatepicker.open({
             initializeValue: new Date('2021/07/31'),
+        });
+        ref.afterClose.subscribe((r) => {
+            if (r == null) return;
+            this.specifyResult = r;
+        });
+    }
+
+    public openDatepickerOutputFormat() {
+        const ref = this.ngbMaterialDatepicker.open({
+            outputFormat: 'yo/MMM/do H:mo:so',
+        });
+        ref.afterClose.subscribe((r) => {
+            if (r == null) return;
+            this.outputFormatResult = r;
         });
     }
 }
